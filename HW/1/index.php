f4e81ebb48ee4e0464f57a857b864978cc039b56
Чулпан Хайруллина <chulpan.khairullina@gmail.com>
2021-02-23 13:34:23 +0300

    first commit

---
+++ HW/1/index.php
@@ -0,0 +1,88 @@
+<?php
+
+if (isset($_REQUEST["submit"])) {
+    $code = $_REQUEST["brainfuck_code"];
+    $parameter = $_REQUEST["param"];
+
+    $code_str = str_split($code);
+    $parameter_str = str_split($parameter);
+
+    check($code_str, $parameter_str);
+} else {
+    include ("hw_1.html");
+}
+
+function check ($code_str, $parameter_str) {
+    $current = 0;
+    $result = array(0);
+    $param = 0;
+
+    $opened_brackets = 0;
+    $closed_brackets = 0;
+
+    for ($i = 0; $i < count($code_str); ++$i) {
+        switch ($code_str[$i]) {
+            case ">" :
+                $current++;
+
+                if (!isset($result[$current])) {
+                    $result[$current] = 0;
+                }
+
+                break;
+            case "<" :
+                $current--;
+
+                if (!isset($result[$current])) {
+                    $result[$current] = 0;
+                }
+
+                break;
+            case "+" :
+                $result[$current]++;
+                break;
+            case "-" :
+                $result[$current]--;
+                break;
+            case "." :
+                print(chr($result[$current]));
+                break;
+            case "," :
+                $result[$current] = ord($parameter_str[$param]);
+                $param++;
+                break;
+            case "[" :
+                if ($result[$current] == 0) {
+                    $opened_brackets++;
+
+                    while ($opened_brackets != 0) {
+                        $i++;
+
+                        if ($code_str[$i] == "[") {
+                            $opened_brackets++;
+                        } else if ($code_str[$i] == "]") {
+                            $opened_brackets--;
+                        }
+                    }
+                }
+
+                break;
+            case "]" :
+                if ($result[$current] != 0) {
+                    $closed_brackets++;
+
+                    while ($closed_brackets != 0) {
+                        $i--;
+
+                        if ($code_str[$i] == "]") {
+                            $closed_brackets++;
+                        } else if ($code_str[$i] == "[") {
+                            $closed_brackets--;
+                        }
+                    }
+                }
+
+                break;
+        }
+    }
+}
