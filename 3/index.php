7dccf6d45c92d1f2109ea3f4df2de89becfc232a
Чулпан Хайруллина <chulpan.khairullina@gmail.com>
2021-04-11 00:55:43 +0300

    hw3 - fixed

--- 3/index.php
+++ 3/index.php
@@ -1,36 +1,27 @@
 <?php
-if (isset($_REQUEST["submit"])) {
-    $strings = $_REQUEST["strings"];
-    $separate_strings = explode("\n", $strings);
-    $sort_strings = sort_str($separate_strings);
+include("hw3.html");
 
-    foreach ($sort_strings as $value) {
-        echo $value . "<br />";
-
-    }
-} else {
-    include "hw3.html";
+if (!isset ($_REQUEST['text'])) {
+    return;
 }
 
-function sort_str($separate_strings) {
-    $sort_str = [];
+$text = $_REQUEST['text'];
+$strings = explode(PHP_EOL, $text);
 
-    for ($i = 0; $i < count($separate_strings); $i++) {
-        $separate_strings[$i] = explode(' ', $separate_strings[$i]);
-        array_push($sort_str, $separate_strings[$i]);
-        shuffle($separate_strings[$i]);
-        array_push($sort_str, $separate_strings[$i]);
-    }
+for ($i = 0; $i < sizeof($strings); $i++) {
+    $arr = explode(" ", $strings[$i]);
+    shuffle($arr);
+    $strings[$i] = implode(" ", $arr);
+}
 
-    usort($sort_str, function ($n1, $n2){
-        //здесь почему-то ругается на пробелы
-       if ($n1[1] < $n2[1]) return -1;
-       else return 1;
-    });
+$all_strings = array_merge($strings, explode( PHP_EOL, $text));
 
-    for ($i = 0; $i < count($sort_str); $i++) {
-        $sort_str[$i] = implode(" ", $sort_str[$i]);
-    }
+function comparator ($str1, $str2) {
+    return strcmp(explode(" ", $str1)[1], explode(" ", $str2)[1]);
+}
 
-    return $sort_str;
+uasort($all_strings, 'comparator');
+
+foreach ($all_strings as $string) {
+    echo $string."<br>";
 }
