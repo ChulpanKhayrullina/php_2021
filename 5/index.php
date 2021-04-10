c9e377f106050f88d78631eebe57c3c6a4d6a3e8
Чулпан Хайруллина <chulpan.khairullina@gmail.com>
2021-04-11 00:10:56 +0300

    hw5 - fixed

--- 5/index.php
+++ 5/index.php
@@ -1,71 +1,27 @@
 <?php
-if (isset($_REQUEST["check"])) {
-    $password = $_REQUEST["password"];
+include("hw5.html");
 
-    $length = '/.{10}/';
-    $required_two_symbols = '/(?=..*[0-9])(?=..*[%$#_*])(?=..*[a-z])(?=..*[A-Z])/';
-    $three_near = '/ ([0-9]{4,})|([%$#_*]{4,})|([a-z]{4,})|([A-Z]{4,})/';
-
-    if (preg_match($length, $password) && preg_match($required_two_symbols, $password) && (!preg_match($three_near, $password))) {
-        print ("ПЕРФЕКТО!");
-    } else {
-        check_first_rule($length, $password);
-        echo("<br/>" . "<br/>");
-        check_second_rule($required_two_symbols, $password);
-        echo("<br/>" . "<br/>");
-        check_third_rule($three_near, $password);
-    }
-} else {
-    include "hw5.html";
+if (!isset ($_REQUEST['password'])) {
+    return;
 }
 
-function check_first_rule($length, $password) {
-    if (!preg_match($length, $password)) {
-        print("Пароль должен содержать более 10 символов ;)");
-    }
-}
+$password = $_REQUEST['password'];
 
-function check_second_rule($required_two_symbols, $password) {
-    if (!preg_match($required_two_symbols, $password)) {
-        print ("Пароль должен содержать хотя бы по 2 символа из каждой категории: -- ");
+if (check($password) === "correct") echo "ПЕРФЕКТ!";
+else echo check($password);
 
-        if (!preg_match('/..*[0-9]/', $password)) {
-            print (" проверьте наличие цифр ");
-        }
+function check($word): string {
+    if(!preg_match("/.{10,}/", $word)) return "Пароль должен содержать не менее 10-ти символов";
 
-        if (!preg_match('/..*[a-z]/', $password)) {
-            print (" проверьте наличие строчных букв ");
-        }
+    else if (!preg_match("/.*[a-z].*[a-z].*/",$word)) return "Пароль должен содержать хотя бы 2-е строчных буквы";
 
-        if (!preg_match('/..*[A-Z]/', $password)) {
-            print (" проверьте наличие прописных букв ");
-        }
+    else if (!preg_match("/.*[A-Z].*[A-Z].*/",$word)) return "Пароль должен содержать хотя бы 2-е прописных буквы";
 
-        if (!preg_match('/..*[%$#_*]/', $password)) {
-            print (" проверьте наличие спец. символов ");
-        }
-    }
+    else if (!preg_match("/.*[0-9].*[0-9].*/",$word)) return "Пароль должен содержать хотя бы 2-е цифры";
 
-}
+    else if (!preg_match("/.*[%$#_*].*[%$#_*].*/",$word)) return "Пароль должен содержать хотя бы 2 специальных символа: %$#_*";
 
-function check_third_rule($three_near, $password) {
-    if (preg_match($three_near, $password)) {
-        print ("Пароль не должен содержать более чем 3 символа любой категории подряд -- ");
+    else if (preg_match("/.*([a-z]{4,})|([A-Z]{4,})|([0-9]{4,})|([%$#_*]{4,}).*/",$word)) return "Пароль содержит более 3 символов одной категории подряд";
 
-        if (preg_match('/[0-9]{4,}/', $password)) {
-            print (" проверьте цифры ");
-        }
-
-        if (preg_match('/[a-z]{4,}/', $password)) {
-            print (" проверьте строчные буквы ");
-        }
-
-        if (preg_match('/[A-Z]{4,}/', $password)) {
-            print (" проверьте прописные буквы ");
-        }
-
-        if (preg_match('/[%$#_*]{4,}/', $password)) {
-            print (" проверьте спец. символы ");
-        }
-    }
+    else return "correct";
 }
